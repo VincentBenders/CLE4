32af3ec36493d3f4e2e93dc26513c72b1c33415f
GinusHR <1074730@hr.nl>
2024-06-11 15:49:50 +0200

    Startscreen is functioneel

--- src/js/scenes/fightscreen.js
+++ src/js/scenes/fightscreen.js
@@ -1,8 +1,8 @@
 import { Keys, Scene, Vector } from "excalibur";
-import { Label, FontUnit, Font} from "excalibur";
+import { Label, FontUnit, Font } from "excalibur";
 
 export class FightScreen extends Scene {
-onActivate(){
-console.log('fightscreen');
-}
+    onActivate() {
+        console.log('fightscreen');
+    }
 }
--- src/js/scenes/startscreen.js
+++ src/js/scenes/startscreen.js
@@ -1,10 +1,51 @@
-import { Keys, Scene, Vector } from "excalibur";
-import { Label, FontUnit, Font} from "excalibur";
+import { Color, Keys, Scene, Vector } from "excalibur";
+import { Label, FontUnit, Font } from "excalibur";
 
 export class StartScreen extends Scene {
-    onInitialize(engine){
+    onInitialize(engine) {
         console.log("start screen!")
+       
     }
+
+    onActivate() {
+        this.startLabel = new Label({
+            text: 'WELKOM TO HEFPARK',
+            pos: new Vector(350, 150),
+            font: new Font({
+                family: 'Arial',
+                size: 60,
+                unit: FontUnit.Px,
+                color: Color.White
+            })
+        })
+        this.add(this.startLabel)
+
+        this.startButton = new Label({
+            text: 'PRESS SPACE TO START',
+            pos: new Vector(425, 250),
+            font: new Font({
+                family: 'Arial',
+                size: 30,
+                unit: FontUnit.Px,
+                color: Color.White
+            })
+        })
+        this.add(this.startButton)
+
+        this.creditsLabel = new Label({
+            text: 'Made by Sander Lanmeter, Juno Craane, Sil van Gemeren, Vincent Benders, Chris Tang, Kasper de jong, Mathijs van der Meijde en Ginus van der Zee',
+            pos: new Vector(120, 650),
+            font: new Font({
+                family: 'Arial',
+                size: 15,
+                unit: FontUnit.Px,
+                color: Color.White
+            })
+        })
+        this.add(this.creditsLabel)
+        
+    }
+
 
     onPostUpdate(engine){
         if (engine.input.keyboard.wasPressed(Keys.Space)) {
