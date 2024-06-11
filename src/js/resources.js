7a702f0a0d0f5ebeb44cad1f037564839073c965
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-06-11 17:40:34 +0200

    Excalibur.js installed

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,12 +1,8 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
-// voeg hier jouw eigen resources toe
 const Resources = {
     Fish: new ImageSource('images/fish.png')
 }
-
-
-
 
 const ResourceLoader = new Loader()
 for (let res of Object.values(Resources)) {
