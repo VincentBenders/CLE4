c99a6f59cbb63d1ca152c1be74982aa798c295b3
GinusHR <1074730@hr.nl>
2024-06-12 14:18:57 +0200

    comments met info voor timeoutscreen en choach

--- src/js/scenes/timeoutscreen.js
+++ src/js/scenes/timeoutscreen.js
@@ -1,6 +1,12 @@
 import { Scene } from "excalibur";
 
 export class TimeOutScreen extends Scene {
+    constructor() {
+        super()
+        // In de haakjes naast constructor moeten variabelen komen voor health en aantal downs voor de Boss en player
+    }
+
+
     onActivate() {
         console.log('tijd voor een pauze');
     }
@@ -10,4 +16,6 @@ export class TimeOutScreen extends Scene {
     // Ik heb om dit te maken nog een aparte background nodig.
     // Ook heb ik sprites nodig voor de Coach en aparte sprites voor de player en de Boss.
     // Er komt een tekstvak waarin de tip wordt weergegeven.
+    // Er moet ook een indicatie van welke Boss het is. Zodat de juiste sprites worden gebruikt.
+    // Waarden zoals aantal keer gedowned moeten opgeslagen worden
 }
