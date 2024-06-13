10dc8fa13d155393a92e2988f3ac155fca5b57bc
Vincent Benders <vincentbenders@gmail.com>
2024-06-13 16:07:21 +0200

    attacks have cooldowns

--- src/js/player/player.js
+++ src/js/player/player.js
@@ -1,4 +1,4 @@
-import { Actor, Axes, Buttons, Engine, Keys, Vector } from "excalibur";
+import { Actor, Axes, Buttons, Engine, Keys, Timer, Vector } from "excalibur";
 import { Resources } from "../resources";
 
 export class Player extends Actor {
@@ -11,6 +11,7 @@ export class Player extends Actor {
   isDown;
   isAttacking;
   isDodging;
+  cooldown;
   onInitialize(Engine) {
     this.game = Engine;
     this.graphics.use(Resources.Player.toSprite());
@@ -23,6 +24,16 @@ export class Player extends Actor {
     this.exhuasted = false;
     this.isAttacking = false;
     this.isDodging = false;
+    this.punch = "";
+    this.cooldown = new Timer({
+      fcn: () => {
+        this.isAttacking = false;
+        console.log(this.punch);
+      },
+      repeats: false,
+      interval: 600,
+    });
+    this.scene?.add(this.cooldown);
   }
   onActivate(Engine) {
     this.game = Engine;
@@ -49,21 +60,54 @@ export class Player extends Actor {
     const xValue = engine.mygamepad.getAxes(Axes.LeftStickX);
     const yValue = engine.mygamepad.getAxes(Axes.LeftStickY);
     // this.vel = new Vector(xValue * 100, yValue * 100)
-    console.log("x: ", xValue, "y: ", yValue);
+    // console.log("x: ", xValue, "y: ", yValue);
     // schieten / springen
     // attacks
     if (engine.mygamepad.wasButtonPressed(Buttons.Face1) && yValue === 0) {
-      console.log("left!");
+      if (!this.isAttacking) {
+        // console.log("left!");
+        this.punch = "lower left";
+        //   if (!this.isAttacking) {
+        // console.log("left attack!");
+        this.isAttacking = true;
+        this.cooldown.start();
+      }
+      //   }
     }
     if (engine.mygamepad.wasButtonPressed(Buttons.Face2) && yValue === 0) {
-      console.log("right!");
+      if (!this.isAttacking) {
+        // console.log("right!");
+        this.punch = "lower right";
+        //   if (!this.isAttacking) {
+        // console.log("left attack!");
+        this.isAttacking = true;
+        this.cooldown.start();
+      }
     }
     // upper attacks
     if (engine.mygamepad.wasButtonPressed(Buttons.Face2) && yValue < 0) {
-      console.log("up right!");
+      if (!this.isAttacking) {
+        // console.log("right!");
+        this.punch = "upper right";
+        //   if (!this.isAttacking) {
+        // console.log("left attack!");
+        this.isAttacking = true;
+        this.cooldown.start();
+      }
     }
     if (engine.mygamepad.wasButtonPressed(Buttons.Face1) && yValue < 0) {
-      console.log("up left!");
+      if (!this.isAttacking) {
+        // console.log("left!");
+        this.punch = "upper left";
+        //   if (!this.isAttacking) {
+        // console.log("left attack!");
+        this.isAttacking = true;
+        this.cooldown.start();
+      }
+    }
+    if (engine.mygamepad.wasButtonPressed(Buttons.Face3) && yValue === 0) {
+      console.log("supa!");
+      //   this.isAttacking = true;
     }
 
     if (this.stamina === 0) {
