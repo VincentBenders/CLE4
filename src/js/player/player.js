9310c45bd6010aecb6bde48a2ffa2d5356cde4bb
Vincent Benders <vincentbenders@gmail.com>
2024-06-14 15:31:15 +0200

    activate not needed

--- src/js/player/player.js
+++ src/js/player/player.js
@@ -40,19 +40,23 @@ export class Player extends Actor {
     });
     this.scene?.add(this.cooldown);
   }
-  onActivate(Engine) {
-    this.game = Engine;
-    this.graphics.use(Resources.Player.toSprite());
-    this.pos = new Vector(400, 400);
-    this.stamina = 20;
-    this.health = 100;
-    this.superEnergy = 0;
-    this.downed = 0;
-    this.isDown = false;
-    this.exhuasted = false;
-    this.isAttacking = false;
-    this.isDodging = false;
-  }
+
+  //create function to check if previous scene was switch round
+  //
+
+  // onActivate(Engine) {
+  //   this.game = Engine;
+  //   this.graphics.use(Resources.Player.toSprite());
+  //   this.pos = new Vector(400, 400);
+  //   this.stamina = 20;
+  //   this.health = 100;
+  //   this.superEnergy = 0;
+  //   this.downed = 0;
+  //   this.isDown = false;
+  //   this.exhuasted = false;
+  //   this.isAttacking = false;
+  //   this.isDodging = false;
+  // }
 
   onPreUpdate(engine) {
     if (engine.mygamepad === null) {
