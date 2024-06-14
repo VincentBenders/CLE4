9e46d8f2e3b1b71a5110942571b8c7f28c25421b
Vincent Benders <vincentbenders@gmail.com>
2024-06-14 15:00:20 +0200

    made dodges and isdodging state

--- src/js/player/player.js
+++ src/js/player/player.js
@@ -6,6 +6,8 @@ export class Player extends Actor {
   stamina;
   health;
   superEnergy;
+  punch;
+  dodge;
   exhuasted;
   downed;
   isDown;
@@ -15,7 +17,7 @@ export class Player extends Actor {
   onInitialize(Engine) {
     this.game = Engine;
     this.graphics.use(Resources.Player.toSprite());
-    this.pos = new Vector(400, 400);
+    this.pos = new Vector(700, 700);
     this.stamina = 20;
     this.health = 100;
     this.superEnergy = 0;
@@ -61,7 +63,7 @@ export class Player extends Actor {
     // bewegen
     const xValue = engine.mygamepad.getAxes(Axes.LeftStickX);
     const yValue = engine.mygamepad.getAxes(Axes.LeftStickY);
-
+    console.log("x: ", xValue, "y: ", yValue);
     // attacks
     if (engine.mygamepad.wasButtonPressed(Buttons.Face1) && yValue === 0) {
       if (!this.isAttacking && !this.isDodging) {
@@ -125,14 +127,14 @@ export class Player extends Actor {
         this.cooldown.start();
       }
     }
-    if (xValue === 1) {
+    if (yValue === 1) {
       if (!this.isAttacking && !this.isDodging) {
         this.isDodging = true;
         this.dodge = "duck";
         this.cooldown.start();
       }
     }
-    if (xValue === -1) {
+    if (yValue === -1) {
       if (!this.isAttacking && !this.isDodging) {
         this.dodge = "block";
         this.cooldown.start();
