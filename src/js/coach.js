bb5506beba0a9bd6f98aea7e678764c1a3f4ff83
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-06-20 11:21:48 +0200

    Coach Animation added in coach.js

--- src/js/coach.js
+++ src/js/coach.js
@@ -1,4 +1,4 @@
-import { Actor } from "excalibur";
+import { Actor, SpriteSheet, Animation, range } from "excalibur";
 import { Resources } from "./resources";
 // De functie van de Coach is morele support.
 // Hij geeft je tips over controls, tactieken en de verschillende bosses
@@ -14,7 +14,17 @@ export class Coach extends Actor {
     tips = [];
     onInitialize(pos) {
         
-        this.graphics.use(Resources.CoachFish.toSprite());
+        // this.graphics.use(Resources.CoachFish.toSprite());
+
+        const CoachFish = SpriteSheet.fromImageSource({
+            image: Resources.CoachFish,
+            grid: { rows: 1, columns: 2, spriteWidth: 190, spriteHeight: 329 }
+        });
+
+        const CoachFrames = range(0, 1);
+        const CoachFishAnimation = Animation.fromSpriteSheet(CoachFish, CoachFrames, 500);
+        this.graphics.use(CoachFishAnimation);
+
         this.tips = [
             'Let op je stamina! Als je niks geen stamina hebt kan je niet slaan.', 
             'Sil is een beetje sloom. Maak je geen zorgen om zijn block.',
