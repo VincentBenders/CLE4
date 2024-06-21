95dac02f758742fd2386d2603f617e7dbbc630f4
Sander <sanderlandm@gmail.com>
2024-06-21 16:59:25 +0200

    Fixed switch case

--- src/js/enemies/attack.js
+++ src/js/enemies/attack.js
@@ -28,10 +28,10 @@ export class Attack extends Move {
         }
 
         switch (type) {
-            case 'uppercut' || 'overhead': affectedAreas.front = true; affectedAreas.down = true; break;
-            case 'rightHook' || 'leftCross': affectedAreas.front = true; affectedAreas.left = true; break;
-            case 'leftHook' || 'rightCross': affectedAreas.front = true; affectedAreas.right = true; break;
-            case 'clothesline' || 'earClap': affectedAreas.front = true; affectedAreas.left = true; affectedAreas.right = true; break;
+            case 'uppercut': case 'overhead': affectedAreas.front = true; affectedAreas.down = true; break;
+            case 'rightHook': case 'leftCross': affectedAreas.front = true; affectedAreas.left = true; break;
+            case 'leftHook': case 'rightCross': affectedAreas.front = true; affectedAreas.right = true; break;
+            case 'clothesline': case 'earClap': affectedAreas.front = true; affectedAreas.left = true; affectedAreas.right = true; break;
             case 'leftUppercut': affectedAreas.front = true; affectedAreas.left = true; affectedAreas.down = true; break;
             case 'rightUppercut': affectedAreas.front = true; affectedAreas.right = true; affectedAreas.down = true; break;
         }
