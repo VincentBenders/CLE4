7cf31bae1e3edb74002a41e13446d2c2ba825926
Sander <sanderlandm@gmail.com>
2024-06-21 15:46:03 +0200

    Chris now has enough logic to function like a boss

--- src/js/enemies/chrisBoss.js
+++ src/js/enemies/chrisBoss.js
@@ -1,6 +1,7 @@
 import {Boss} from "./boss.js";
 import {Random} from "excalibur";
 import {animate} from "../resources.js";
+import {Attack} from "./attack.js";
 
 
 export class ChrisBoss extends Boss {
@@ -8,16 +9,22 @@ export class ChrisBoss extends Boss {
     //Properties
     random;
     blockChance;
+    patternLengthMax;
+
 
     constructor() {
         super(125, 'chris');
         this.random = new Random();
 
-        this.nextAttackDelay = 750;
+        this.nextAttackDelay = 350;
 
         this.blockChance = 0.8;
 
-        this.nextPatternDelay = this.random.integer(750, 2250) ;
+        this.nextPatternDelay = this.random.integer(250, 550);
+
+        this.patternLengthMax = 4;
+
+        this.stunnedDuration = 1000;
 
 
         this.setMoves();
@@ -32,8 +39,14 @@ export class ChrisBoss extends Boss {
     setMoves() {
 
 
-        const leftJabAnimation = animate(600, this.spriteSheet, )
+        const leftJabAnimation = animate(800, this.spriteSheet, [70, 71, 72, 73]);
+        this.moves.leftJab = new Attack(20, 'jab', 8, 600, 300, leftJabAnimation, 3);
 
+        const rightJabAnimation = animate(800, this.spriteSheet, [60, 61, 62, 63, 64]);
+        this.moves.rightJab = new Attack(20, 'jab', 8, 700, 300, rightJabAnimation, 4);
+
+        const leftHookAnimation = animate(1200, this.spriteSheet, [84, 85, 86, 87, 88]);
+        this.moves.leftHook = new Attack(35, 'leftHook', 12, 400, 200, leftHookAnimation, 4);
 
 
 
@@ -52,8 +65,19 @@ export class ChrisBoss extends Boss {
         super.setNextPattern();
 
 
+        const patternLength = this.random.integer(1, this.patternLengthMax);
 
-        this.nextPatternDelay = this.random.integer(750, 2250) ;
+        const attackList = Object.entries(this.moves);
+
+        let nextAttacks = this.random.pickSet(attackList, patternLength, true)
+
+        for (const nextAttack of nextAttacks) {
+
+            this.pattern.unshift(nextAttack[1]);
+
+        }
+
+        this.nextPatternDelay = this.random.integer(450, 8250);
 
     }
 
@@ -77,6 +101,13 @@ export class ChrisBoss extends Boss {
         super.postGetUp();
 
         this.blockChance -= 0.15;
+        this.patternLengthMax++;
+
+        this.nextAttackDelay -= 100;
+
+        if (this.nextAttackDelay < 50) {
+            this.nextAttackDelay = 50;
+        }
 
 
     }
